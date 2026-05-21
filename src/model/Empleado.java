b9b23ae356f70878123d14e3939e80c6d355ca1b
yepezmosquerasamuelfhilliph-sketch <yepezmosquerasamuelfhilliph@gmail.com>
2026-05-20 21:05:22 -0500

    Create Empleado.java

---
+++ src/model/Empleado.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Empleado extends Persona {
+
+    private double salario;
+
+    public Empleado(String nombre, int edad, double salario) {
+
+        super(nombre, edad);
+        this.salario = salario;
+
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+
+        if (salario > 0) {
+
+            this.salario = salario;
+
+        }
+
+    }
+
+}
